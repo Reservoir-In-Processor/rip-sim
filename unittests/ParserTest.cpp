ff412e4afe9205e96b883f4dc26286f39fb9cb84
khei4 <kk.asano.luxy@gmail.com>
2023-09-05 15:24:08 +0900

    remove should

--- unittests/ParserTest.cpp
+++ unittests/ParserTest.cpp
@@ -11,17 +11,17 @@ TEST(ParserTest, Basics) {
   EXPECT_TRUE(AP.parseLine());
 
   const auto &Addi = AP.getTokens();
-  EXPECT_EQ(Addi[0], "addi") << "should addi";
-  EXPECT_EQ(Addi[1], "a0") << "should a0";
-  EXPECT_EQ(Addi[2], "a0") << "should a0";
-  EXPECT_EQ(Addi[3], "2047") << "should 2047";
+  EXPECT_EQ(Addi[0], "addi");
+  EXPECT_EQ(Addi[1], "a0");
+  EXPECT_EQ(Addi[2], "a0");
+  EXPECT_EQ(Addi[3], "2047");
 
   EXPECT_TRUE(AP.parseLine());
   const auto &Xori = AP.getTokens();
-  EXPECT_EQ(Xori[0], "xori") << "should xori";
-  EXPECT_EQ(Xori[1], "a0") << "should a0";
-  EXPECT_EQ(Xori[2], "a0") << "should a0";
-  EXPECT_EQ(Xori[3], "4") << "should 4";
+  EXPECT_EQ(Xori[0], "xori");
+  EXPECT_EQ(Xori[1], "a0");
+  EXPECT_EQ(Xori[2], "a0");
+  EXPECT_EQ(Xori[3], "4");
 }
 
 TEST(ParserTest, Labels) {
@@ -37,21 +37,21 @@ TEST(ParserTest, Labels) {
   EXPECT_EQ(AP.getLabels().find(".L3")->second, 0x08);
   EXPECT_TRUE(AP.parseLine());
   const auto &Add = AP.getTokens();
-  EXPECT_EQ(Add[0], "add") << "should add";
-  EXPECT_EQ(Add[1], "a5") << "should a5";
-  EXPECT_EQ(Add[2], "a1") << "should a1";
-  EXPECT_EQ(Add[3], "a2") << "should a2";
+  EXPECT_EQ(Add[0], "add");
+  EXPECT_EQ(Add[1], "a5");
+  EXPECT_EQ(Add[2], "a1");
+  EXPECT_EQ(Add[3], "a2");
   EXPECT_TRUE(AP.parseLine());
   const auto &Mv = AP.getTokens();
-  EXPECT_EQ(Mv[0], "mv") << "should mv";
-  EXPECT_EQ(Mv[1], "a5") << "should a5";
-  EXPECT_EQ(Mv[2], "a0") << "should a0";
+  EXPECT_EQ(Mv[0], "mv");
+  EXPECT_EQ(Mv[1], "a5");
+  EXPECT_EQ(Mv[2], "a0");
   EXPECT_TRUE(AP.parseLine());
   const auto &Bne = AP.getTokens();
-  EXPECT_EQ(Bne[0], "bne") << "should bne";
-  EXPECT_EQ(Bne[1], "a5") << "should a5";
-  EXPECT_EQ(Bne[2], "a2") << "should a2";
-  EXPECT_EQ(Bne[3], ".L3") << "should .L3";
+  EXPECT_EQ(Bne[0], "bne");
+  EXPECT_EQ(Bne[1], "a5");
+  EXPECT_EQ(Bne[2], "a2");
+  EXPECT_EQ(Bne[3], ".L3");
 }
 
 TEST(ParserTest, LoadStore) {
