e25d729171ab1677af4764a67f28452ec1a12c32
Kohei Asano <32860920+khei4@users.noreply.github.com>
2023-09-23 12:24:31 +0900

    implement load/store(unsigned) for halfword, byte (#13)

--- include/Simulator/Memory.h
+++ include/Simulator/Memory.h
@@ -18,6 +18,7 @@ using Address = std::uint64_t;
 const Address DRAM_BASE = 0x8000;
 
 using Byte = std::uint8_t;
+using HalfWord = std::uint16_t;
 using Word = std::uint32_t;
 
 class Memory {
@@ -30,6 +31,10 @@ public:
 
   Memory() : DRAM(DRAM_SIZE, 0) {}
 
+  void writeByte(Address Ad, Byte Val);
+  Byte readByte(Address Ad);
+  void writeHalfWord(Address Ad, HalfWord Val);
+  HalfWord readHalfWord(Address Ad);
   void writeWord(Address Ad, Word Val);
   Word readWord(Address Ad);
 };
