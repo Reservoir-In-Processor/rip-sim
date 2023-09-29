abad42ae947669ea91de7a6465e205f91c6b960f
khei4 <kk.asano.luxy@gmail.com>
2023-09-29 19:43:17 +0900

    add: dumb pipeline

--- include/RIPSimulator/RIPSimulator.h
+++ include/RIPSimulator/RIPSimulator.h
@@ -20,12 +20,20 @@ enum STAGES {
 
 class PipelineStates {
 private:
-  unsigned EXDestReg;
-  unsigned MADestReg;
+  // member are the values we want to dump for every stages
   unsigned FetchedInst;
-  Reg EXResult;
+  // previous cycle values.
+  RegVal EXRdVal;
+  RegVal MARdVal;
+
+  RegVal DERs1Val;
+  RegVal DERs2Val;
+  Address NextPC;
+  // pipeline bubbles are nullptr
   std::shared_ptr<Instruction> Insts[STAGENUM];
+  // better way to invalidate values or stall
   bool AreStall[STAGENUM];
+  bool AreInvalid[STAGENUM];
 
 public:
   PipelineStates(const PipelineStates &) = delete;
@@ -33,6 +41,30 @@ public:
 
   PipelineStates() {}
   void dump() { assert(false && "unimplemented!"); }
+
+  const RegVal &getEXRdVal() { return EXRdVal; }
+  void setEXRdVal(RegVal &V) { EXRdVal = V; }
+
+  const RegVal &getDERs1Val() { return DERs1Val; }
+  void setDERs1Val(const RegVal &V) { DERs1Val = V; }
+
+  const RegVal &getDERs2Val() { return DERs2Val; }
+  void setDERs2Val(const RegVal &V) { DERs2Val = V; }
+
+  const RegVal &getMARdVal() { return MARdVal; }
+  void setMARdVal(const RegVal &V) { MARdVal = V; }
+
+  const unsigned &getFetchedInst() { return FetchedInst; }
+  void setFetchedInst(const unsigned &V) { FetchedInst = V; }
+
+  const Address &getNextPC() { return NextPC; }
+  void setNextPC(const Address &V) { NextPC = V; }
+
+  const bool isStall(const STAGES &S) { return AreStall[S]; }
+  void setStall(const STAGES &S) { AreStall[S] = true; }
+
+  const bool isInvalid(const STAGES &S) { return AreInvalid[S]; }
+  void setInvalid(const STAGES &S) { AreInvalid[S] = true; }
 
   const std::shared_ptr<Instruction> &operator[](unsigned Stage) const {
     assert(Stage < STAGENUM && "Index out of bounds");
@@ -45,6 +77,7 @@ private:
   Memory M;
   unsigned CodeSize;
   Address PC;
+  unsigned CycleNum;
   PipelineStates PS;
   GPRegisters GPRegs;
   std::map<Address, std::unique_ptr<Instruction>> PCInstMap;
@@ -54,7 +87,7 @@ public:
   RIPSimulator &operator=(const RIPSimulator &) = delete;
 
   // move this def to .cpp
-  RIPSimulator(std::istream &is) : PC(DRAM_BASE) {
+  RIPSimulator(std::istream &is) : PC(DRAM_BASE), CycleNum(0) {
     // TODO: parse per 2 bytes for compressed instructions
     char Buff[4];
     // starts from DRAM_BASE
@@ -64,6 +97,7 @@ public:
       unsigned InstVal = *(reinterpret_cast<unsigned *>(Buff));
       // Raw inst
       M.writeWord(P, InstVal);
+      CodeSize += 4;
     }
   }
   GPRegisters &getGPRegs() { return GPRegs; }
@@ -76,7 +110,6 @@ public:
 
   void runFromDRAMBASE() {
     PC = DRAM_BASE;
-    unsigned CycleNum = 0;
     while (true) {
       writeback(GPRegs, PS);
 
