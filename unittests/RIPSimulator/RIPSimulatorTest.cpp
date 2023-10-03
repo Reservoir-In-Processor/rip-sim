63c1b7627d74e16c6e7ad3137c255461fe1d722f
khei4 <kk.asano.luxy@gmail.com>
2023-10-03 16:41:59 +0900

    comment out warned lines

--- unittests/RIPSimulator/RIPSimulatorTest.cpp
+++ unittests/RIPSimulator/RIPSimulatorTest.cpp
@@ -8,10 +8,10 @@ TEST(RIPSimulatorTest, ADDI) {
       0x93, 0x08, 0x30, 0x00, // addi, x17, x0, 3
   };
 
-  const GPRegisters EXPECTED = {{16, 5}, {17, 3}};
+  // const GPRegisters EXPECTED = {{16, 5}, {17, 3}};
   std::stringstream ss;
   ss.write(reinterpret_cast<const char *>(BYTES), sizeof(BYTES));
-  RIPSimulator RSim(ss);
+  // RIPSimulator RSim(ss);
 
   // FIXME: pass test and compare each cycle states if possible
 }
