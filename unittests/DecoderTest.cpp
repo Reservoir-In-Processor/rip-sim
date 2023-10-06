0c0b094367c2ec76163ba10eae428d8e87753d41
Kohei Asano <32860920+khei4@users.noreply.github.com>
2023-10-06 10:09:27 +0900

    implement ZiCsr decoder (#32)
    * add: decoding for csr
    
    * add: missing break and unit tests for that
    
    * make: decoder test to common
    
    * add: test for csr decoder

---
+++ unittests/DecoderTest.cpp
@@ -0,0 +1,149 @@
+#include "Decoder.h"
+#include <gtest/gtest.h>
+#include <memory>
+// ecall
+TEST(DecoderTest, ECALL) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x00, 0x00, 0x00, // ecall
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "ecall");
+}
+// ebreak
+TEST(DecoderTest, EBREAK) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x00, 0x10, 0x00, // ebreak
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "ebreak");
+}
+// uret,
+// 00000000 00100000 00000000 01110011
+TEST(DecoderTest, URET) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x00, 0x20, 0xc0, // uret
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "uret");
+}
+// sret,
+TEST(DecoderTest, SRET) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x00, 0x20, 0x10, // sret
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "sret");
+}
+
+TEST(DecoderTest, MRET) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x00, 0x20, 0x30, // mret
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "mret");
+}
+
+TEST(DecoderTest, CSRRCI) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x70, 0x00, 0xc0, // csrrci x0, cycle, 0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrci");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
+// csrrsi
+
+TEST(DecoderTest, CSRRSI) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x60, 0x00, 0xc0, // csrrsi x0, cycle, 0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrsi");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
+
+TEST(DecoderTest, CSRRWI) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x50, 0x00, 0xc0, // csrrwi x0, cycle, 0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrwi");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
+
+TEST(DecoderTest, CSRRC) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x30, 0x00, 0xc0, // csrrw x0, cycle, x0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrc");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
+
+TEST(DecoderTest, CSRRS) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x20, 0x00, 0xc0, // csrrs x0, cycle, x0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrs");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
+
+TEST(DecoderTest, CSRRW) {
+  unsigned char INST_BYTES[] = {
+      0x73, 0x10, 0x00, 0xc0, // csrrw x0, cycle, x0
+  };
+
+  unsigned InstVal = *(reinterpret_cast<unsigned *>(INST_BYTES));
+
+  Decoder Dec;
+  std::unique_ptr<Instruction> InstPtr = Dec.decode(InstVal);
+  EXPECT_EQ(InstPtr->getMnemo(), "csrrw");
+  EXPECT_EQ(InstPtr->getRd(), 0);
+  EXPECT_EQ(InstPtr->getRs1(), 0);
+}
