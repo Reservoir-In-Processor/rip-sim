605475e2f50aa0951b3619592c687157eb7cd927
khei4 <kk.asano.luxy@gmail.com>
2023-10-17 17:42:31 +0900

    remove: obsolete diff

--- rip-riscvtests/RIPRiscvTests.cpp
+++ rip-riscvtests/RIPRiscvTests.cpp
@@ -8,65 +8,6 @@
 
 class RIPRiscvTests : public ::testing::TestWithParam<std::string> {};
 
-void RIPSimulator::runRiscvTests() {
-  PC = DRAM_BASE;
-
-  while (true) {
-
-    if (PS.getPCs(STAGES::WB) == 0x804c) {
-      break;
-    }
-    // actual fetch and decode
-    if (!PS.isStall(STAGES::IF)) {
-      auto InstPtr = Dec.decode(Mem.readWord(PC));
-      PS.proceedPC(PC);
-      if (InstPtr) {
-        PC += 4;
-      }
-      PS.proceed(std::move(InstPtr));
-    } else {
-      PS.proceedPC(-1);
-      PS.proceed(nullptr);
-    }
-
-    PS.clearStall();
-
-    std::optional<Exception> E = std::nullopt;
-    if (PS[STAGES::WB] != nullptr)
-      writeback(GPRegs, PS);
-    if (PS[STAGES::MA] != nullptr)
-      memoryaccess(Mem, PS);
-    if (PS[STAGES::EX] != nullptr)
-      E = exec(PS);
-    if (PS[STAGES::DE] != nullptr)
-      decode(GPRegs, PS);
-    if (PS[STAGES::IF] != nullptr)
-      fetch(Mem, PS);
-
-    // Exception handling
-    // TODO: move those handler to some functions i.e. take_trap
-    if (E && !handleException(*E))
-      break;
-
-    PS.fillBubble();
-
-    if (PS.isEmpty()) {
-      break;
-    }
-    // FIXME: might this be wrong if branch prediction happens.
-    if (auto NextPC = PS.takeBranchPC()) {
-      std::cerr << std::hex << "Branch from " << PC << " to ";
-      PC = *NextPC;
-      std::cerr << std::hex << PC << "\n";
-    }
-    CycleNum++;
-#ifdef DEBUG
-    PS.dump();
-    dumpGPRegs();
-#endif
-  }
-}
-
 std::vector<std::string> getBinFilesWithPrefix(const std::string &directory,
                                                const std::string &prefix) {
   std::vector<std::string> binFiles;
