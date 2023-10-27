bc48ec030639e67d6a8010b5b42578e3424b898a
hirayamy <hirayama.yuki.cp@icloud.com>
2023-10-27 12:48:45 +0900

    use getPCs

--- unittests/RIPSimulator/BranchPredictorTest.cpp
+++ unittests/RIPSimulator/BranchPredictorTest.cpp
@@ -239,6 +239,7 @@ TEST(RIPSimulatorTest, GSHARE_CHECKPRED) {
   std::cerr << gsharep->getBranchHistory() << "\n";
 
   RIPSimulator RSim(ss, std::move(bp));
+  auto &PS = RSim.getPipelineStates();
 
   DEBUG_ONLY(std::cerr << "------------ loop1 ------------"
                        << "\n");
@@ -261,7 +262,8 @@ TEST(RIPSimulatorTest, GSHARE_CHECKPRED) {
   EXPECT_EQ(RSim.getBPPred(), 0); // should be hit in 2c
 
   EXPECT_EQ(gsharep->getBranchHistory(), 0b1);
-  EXPECT_EQ(gsharep->getBHTIndex(0x802c), 10); // FIXME: how to get PC in DE?
+
+  EXPECT_EQ(gsharep->getBHTIndex(PS.getPCs(STAGES::DE)), 10);
 
   DEBUG_ONLY(std::cerr << "------------ loop2 ------------"
                        << "\n");
@@ -282,7 +284,7 @@ TEST(RIPSimulatorTest, GSHARE_CHECKPRED) {
   EXPECT_EQ(RSim.getBPPred(), 0); // should be hit in 2c
 
   EXPECT_EQ(gsharep->getBranchHistory(), 0b100001);
-  EXPECT_EQ(gsharep->getBHTIndex(0x802c), 42);
+  EXPECT_EQ(gsharep->getBHTIndex(PS.getPCs(STAGES::DE)), 42);
 
   DEBUG_ONLY(std::cerr << "------------ loop3 ------------"
                        << "\n");
@@ -301,7 +303,7 @@ TEST(RIPSimulatorTest, GSHARE_CHECKPRED) {
 
   RSim.proceedNStage(5);          // NS=91
   EXPECT_EQ(RSim.getBPPred(), 0); // should be miss in 2c
-  EXPECT_EQ(gsharep->getBHTIndex(0x802c), 42);
+  EXPECT_EQ(gsharep->getBHTIndex(PS.getPCs(STAGES::DE)), 42);
 
   RSim.run();
   EXPECT_EQ(gsharep->getBranchHistory(), 0b0001000011);
