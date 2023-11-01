3ee9d640b091edd56277f5290810e0e0924ae5d1
hirayamy <54273214+hirayamy@users.noreply.github.com>
2023-11-01 14:13:34 +0900

    pass dhrystone with bp (#103)

--- rip-dhrystone/RIPDhrystoneTest.cpp
+++ rip-dhrystone/RIPDhrystoneTest.cpp
@@ -6,12 +6,15 @@
 #include <string>
 #include <vector>
 
-TEST(RIPDhrystoneTest, DhryStone) {
+TEST(RIPDhrystoneTest, DhryStone) { // FIXME: should it be separated?
   std::string FileName = "../rip-tests/dhry.bin";
   auto Files = std::ifstream(FileName);
   // FIXME: access on above sp initial value happens, what is the requirements
   // for DRAMSize, DRAMBase, and sp init value?
-  RIPSimulator RSim(Files, /*BP = */ nullptr, /*DRAMSize = */ 1LL << 29,
+  std::unique_ptr<BranchPredictor> bp =
+      std::make_unique<OneBitBranchPredictor>();
+
+  RIPSimulator RSim(Files, /*BP = */ std::move(bp), /*DRAMSize = */ 1LL << 29,
                     /*DRAMBase = */ 0x0000,
                     /*SPIValue = */ 1LL << 25);
   auto &PS = RSim.getPipelineStates();
