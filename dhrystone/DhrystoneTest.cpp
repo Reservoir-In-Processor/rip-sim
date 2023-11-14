98dc5c256f44aac014b6ecf91b65d6ca46f9e796
Kohei Asano <32860920+khei4@users.noreply.github.com>
2023-11-14 16:23:11 +0900

    Revert "fixup the wrong merge of 111" (#116)

--- dhrystone/DhrystoneTest.cpp
+++ dhrystone/DhrystoneTest.cpp
@@ -11,23 +11,10 @@ TEST(DhrystoneTest, DhryStone) {
   auto Files = std::ifstream(FileName);
   // FIXME: access on above sp initial value happens, what is the requirements
   // for DRAMSize, DRAMBase, and sp init value?
-  Simulator Sim(Files, /*DRAMSize = */ 1LL << 28,
-                /* DRAMBase = */ 0x8000,
-                /* SPIvalue = */ 1LL << 25);
+  Simulator Sim(Files, /*DRAMSize = */ 1LL << 29, /*DRAMBase = */ 0x0000,
+                /*SPIValue = */ 1LL << 25);
   Sim.run();
   Sim.dumpGPRegs();
   const Address PC = Sim.getPC();
-  EXPECT_EQ(PC, 0x8490) << "PC unmatched!\n";
-}
-
-TEST(DhrystoneTest, DhryStoneBareMetal) {
-  std::string FileName = "../rip-tests/dhry-baremetal.bin";
-  auto Files = std::ifstream(FileName);
-  // FIXME: access on above sp initial value happens, what is the requirements
-  // for DRAMSize, DRAMBase, and sp init value?
-  Simulator Sim(Files, /*DRAMSize = */ 1LL << 28);
-  Sim.run();
-  Sim.dumpGPRegs();
-  const Address PC = Sim.getPC();
-  EXPECT_EQ(PC, 0x8084) << "PC unmatched!\n";
+  EXPECT_EQ(PC, 0x4a4) << "PC unmatched!\n";
 }
