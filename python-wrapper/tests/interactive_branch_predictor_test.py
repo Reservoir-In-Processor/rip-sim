68200ab246ecda69ca9487b9493494e8019cf8f9
khei4 <kk.asano.luxy@gmail.com>
2023-12-09 18:19:01 +0900

    rename python test correctly

---
+++ python-wrapper/tests/interactive_branch_predictor_test.py
@@ -0,0 +1,28 @@
+from rip_simulator.rip_simulator import RIPSimulator, BranchPredKind
+from pathlib import Path
+
+
+def test_dhrystone_baremetal_interactive_gshare():
+    rsim = RIPSimulator(
+        Path("rip-tests/dhry-baremetal.bin"),
+        BranchPredKind.Interactive,
+        output_sim_err=False,
+    )
+    pss = []
+    learns = []
+    cycle = 0
+    while True:
+        res = rsim.proceed()
+        if res is None:
+            break
+        if res["Kind"] == "PipelineStates":
+            cycle += 1
+            pss.append(res)
+        elif res["Kind"] == "Predict":
+            # predict true if cycle % 2 == 0, false otherwise.
+            rsim.send_data('{ "PredRes": ' + str(cycle % 2 == 0) + " }")
+        elif res["Kind"] == "Learn":
+            learns.push(res)
+        else:
+            assert False, "unreachable!"
+    assert pss[-1]["EX"]["PC"] == 0x8084, "end PC unmatched!"
