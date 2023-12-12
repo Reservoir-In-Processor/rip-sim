ffeb7e43c2dfc22dffda76ca5a60b3838f5faedc
khei4 <kk.asano.luxy@gmail.com>
2023-12-12 15:10:40 +0900

    tmp

---
+++ python-wrapper/reservoir_branch_predictor/esn_online.py
@@ -0,0 +1,178 @@
+import numpy as np
+from scipy import linalg
+
+np.random.seed(0)
+
+
+class ESNOnline:
+    def __init__(
+        self,
+        reservoir_dim,
+        input_dim,
+        output_dim,
+        lr=0.5,
+        sr=0.9,
+        input_scaling=0.1,
+        activation=np.tanh,
+        threshold=0.5,
+        epochs=1,
+    ):
+        self.reservoir_dim = reservoir_dim
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.lr = lr
+        self.input_scaling = input_scaling
+        self.activation = activation
+        self.current_state = np.empty(shape=(self.reservoir_dim, 1))
+        self.epochs = epochs
+        self.threshold = threshold
+
+        # Initialize weights
+        self.Win = np.random.uniform(
+            low=-input_scaling, high=input_scaling, size=[self.reservoir_dim, input_dim]
+        )
+        self.Wrec = np.random.normal(
+            loc=0, scale=1, size=(self.reservoir_dim, self.reservoir_dim)
+        )
+        self.Wrec = (
+            self.Wrec * sr / max(abs(linalg.eigvals(self.Wrec)))
+        )  # scale spectral radius
+
+        self.Wout = np.zeros([output_dim, reservoir_dim])
+
+    def predict(self, inputs: np.ndarray) -> bool:
+        raise NotImplementedError
+
+    def train(self, result: bool) -> None:
+        raise NotImplementedError
+
+    def get_next_state(self, input, current_state) -> np.ndarray:
+        raise NotImplementedError
+
+
+class ESN_RLS(ESNOnline):
+    # "リザバーコンピューティング", 田中剛平著, P. 64との対応
+    # d    <=> Y_train
+    # λ    <=> f_factor
+    # Nx   <=> self.reservoir_dim
+    # x(n) <=> self.current_state
+    # current_state.shape = (Nx, 1)
+    # Win.shape = (Nx, Nu)
+    # Wout.shape = (1, Nx)
+    # P.shape = (Nx, Nx)
+    # g.shape = (Nx, 1)
+    # v.shape = (1, 1)
+    def __init__(
+        self,
+        reservoir_dim,
+        input_dim,
+        output_dim,
+        lr=0.8,
+        sr=0.8,
+        input_scaling=0.1,
+        activation=np.tanh,
+        epochs=1,
+    ):
+        super().__init__(
+            reservoir_dim,
+            input_dim,
+            output_dim,
+            lr,
+            sr,
+            input_scaling,
+            activation,
+            epochs,
+        )
+
+    def train(self, Y_train, f_factor=0.5, delta=0.01):
+        P = 1 / delta * np.identity(self.reservoir_dim)
+        for i in range(self.epochs):
+            v = Y_train - np.matmul(self.Wout, self.current_state)
+            # np.matmul(self.current_state, P) ... (1, Nx)
+            g = (1 / f_factor * np.matmul(P, self.current_state)) / (
+                1
+                + 1
+                / f_factor
+                * np.matmul(self.current_state.T, np.matmul(P, self.current_state))
+            )
+            # np.matmul(self.current_state, P) ... (1, Nx)
+            P = 1 / f_factor * (P - np.matmul(g, np.matmul(self.current_state.T, P)))
+
+            self.Wout = self.Wout + np.matmul(v, g.T)
+
+    def predict(self, inputs: np.ndarray) -> bool:
+        self.current_state = self.get_next_state(inputs)
+        print("nonzero currentstate: ", (self.current_state == 0).sum())
+        # print("Wout: ", self.Wout)
+        # print("currentstate: ", self.current_state)
+        pred: np.ndarray = np.matmul(self.Wout, self.current_state)
+        print(pred)
+        assert pred.shape == (1, 1), f"pred.shape: {pred.shape} should be (1, 1)"
+        # print("pred: ", pred[0, 0])
+        pred_class: bool = self.threshold < pred[0, 0]
+        return pred_class
+
+    def get_next_state(self, input):
+        next_state = (1 - self.lr) * self.current_state + self.lr * self.activation(
+            np.matmul(self.Win, input) + np.matmul(self.Wrec, self.current_state)
+        )
+        return next_state
+
+
+class ESN_LMS(ESNOnline):
+    # "リザバーコンピューティング", 田中剛平著, P. 63との対応
+    # d    <=> Y_train
+    # y    <=> pred
+    # λ    <=> f_factor
+    # Nu <=> input_dim
+    # Nx   <=> self.reservoir_dim
+    # x(n) <=> self.current_state
+
+    # current_state.shape = (Nx, 1)
+    # Win.shape = (Nx, Nu)
+    # Wout.shape = (1, Nx)
+
+    def __init__(
+        self,
+        reservoir_dim,
+        input_dim,
+        output_dim,
+        lr=0.8,
+        sr=0.8,
+        input_scaling=0.1,
+        ridge=1e-7,
+        eta=1e-1,
+        activation=np.tanh,
+        epochs=1,
+    ):
+        super().__init__(
+            reservoir_dim,
+            input_dim,
+            output_dim,
+            lr,
+            sr,
+            input_scaling,
+            activation,
+            epochs,
+        )
+        self.ridge = ridge
+        self.eta = eta
+
+    def train(self, Y_train):
+        for _ in range(self.epochs):
+            pred = np.matmul(self.Wout, self.current_state)
+            error = Y_train - pred
+            self.Wout = self.Wout + self.eta * np.matmul(error, self.current_state.T)
+
+    def predict(self, inputs: np.ndarray) -> bool:
+        self.current_state = self.get_next_state(inputs)
+        pred: np.ndarray = np.matmul(self.Wout, self.current_state)
+        assert pred.shape == (1, 1), f"pred.shape: {pred.shape} should be (1, 1)"
+        pred_class: bool = self.threshold < pred[0, 0]
+        return pred_class
+
+    def get_next_state(self, input):
+        next_state = (1 - self.lr) * self.current_state + self.lr * self.activation(
+            np.matmul(self.Win, input) + np.matmul(self.Wrec, self.current_state)
+        )
+        return next_state
--- python-wrapper/tests/online_reservoir_branch_predictor_test.py
+++ python-wrapper/tests/online_reservoir_branch_predictor_test.py
@@ -2,65 +2,13 @@ from rip_simulator.rip_simulator import RIPSimulator, BranchPredKind
 from pathlib import Path
 
 import numpy as np
-from scipy import linalg
+from tqdm import tqdm
+from reservoir_branch_predictor.esn_online import ESN_LMS, ESN_RLS
 
 
-class ESNOnline:
-    def __init__(
-        self,
-        reservoir_dim,
-        input_dim,
-        output_dim,
-        lr=0.5,
-        sr=0.9,
-        input_scaling=0.1,
-        ridge=1e-7,
-        activation=np.tanh,
-    ):
-        self.reservoir_dim = reservoir_dim
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.lr = lr
-        self.input_scaling = input_scaling
-        self.activation = activation
-        self.ridge = ridge
-
-        self.log_reservoir_nodes = np.empty(shape=(self.reservoir_dim))
-
-        # Initialize weights
-        # FIXME: ここで重みをランダムにしない
-        self.Win = np.random.uniform(
-            low=-input_scaling, high=input_scaling, size=[self.input_dim, reservoir_dim]
-        )
-        self.Wrec = np.random.normal(
-            loc=0, scale=1, size=(self.reservoir_dim, self.reservoir_dim)
-        )
-        self.Wrec = (
-            self.Wrec * sr / max(abs(linalg.eigvals(self.Wrec)))
-        )  # scale spectral radius
-
-        self.Wout = np.zeros([reservoir_dim, output_dim])
-
-    def predict(self, inputs: np.ndarray) -> bool:
-        # FIXME:" predict
-
-        return True
-
-    def train(self, result: bool) -> None:
-        # FIXME: update w_out
-        return
-
-    def get_next_state(self, input, current_state) -> np.ndarray:
-        # FIXME:
-        return
-
-
-# states =
-
-
-def test_online_reservoir_bp():
+def test_online_reservoir_bp_lms():
     rsim = RIPSimulator(
-        Path("rip-tests/dhry-baremetal.bin"),
+        Path("rip-tests/dhry.bin"),
         BranchPredKind.Interactive,
         output_sim_err=False,
     )
@@ -68,8 +16,8 @@ def test_online_reservoir_bp():
     hit = 0
     prev_pred = None
     stage_num = 0
-    rbp = ESNOnline(reservoir_dim=100, input_dim=1, output_dim=1)
-    while True:
+    rbp = ESN_LMS(reservoir_dim=100, input_dim=1, output_dim=1)
+    for _ in tqdm(range(124791 * 2)):
         res = rsim.proceed()
         if res is None:  # end of simulation
             break
@@ -80,7 +28,7 @@ def test_online_reservoir_bp():
             pass
         elif res["Kind"] == "Predict":
             # predict here, currently input is only previous prediction result.
-            inputs = np.array([res["PrevPred"]])
+            inputs = np.array([res["PrevPred"]]).reshape([1, 1])
             predict = rbp.predict(inputs)
             rsim.predict(predict)
             prev_pred = predict
@@ -88,14 +36,58 @@ def test_online_reservoir_bp():
             branch_num += 1
             # train here
             rbp.train(res["Cond"])
-            print("prev_pred: ", prev_pred)
-            print("res[Cond]: ", res["Cond"])
             if prev_pred is not None and (
                 (prev_pred and res["Cond"]) or (not prev_pred and not res["Cond"])
             ):
                 hit += 1
         else:
             assert False, "unreachable!"
+
+    print("hit: ", hit)
+    print("branch_num: ", branch_num)
+    print("stage_num: ", stage_num)
+    print("accuracy: ", hit / branch_num)
+    # assert pss[-1]["EX"]["PC"] == 0x8084, "end PC unmatched!"
+
+
+def test_online_reservoir_bp_rls():
+    rsim = RIPSimulator(
+        Path("rip-tests/dhry.bin"),
+        BranchPredKind.Interactive,
+        output_sim_err=False,
+    )
+    branch_num = 0
+    hit = 0
+    prev_pred = None
+    stage_num = 0
+    rbp = ESN_RLS(reservoir_dim=100, input_dim=1, output_dim=1)
+    for _ in tqdm(range(124791 * 2)):
+        res = rsim.proceed()
+        if res is None:  # end of simulation
+            break
+        if res["Kind"] == "PipelineStates":
+            stage_num += 1
+            # FIXME: update reservoir state by pipeline states?
+            pass
+        elif res["Kind"] == "Predict":
+            # predict here, currently input is only previous prediction result.
+            inputs = np.array([res["PrevPred"]]).reshape([1, 1])
+            predict = rbp.predict(inputs)
+            rsim.predict(predict)
+            prev_pred = predict
+            print("predict: ", predict)
+        elif res["Kind"] == "Learn":
+            print("res[Cond]: ", res["Cond"])
+            branch_num += 1
+            # train here
+            rbp.train(res["Cond"])
+            if prev_pred is not None and (
+                (prev_pred and res["Cond"]) or (not prev_pred and not res["Cond"])
+            ):
+                hit += 1
+        else:
+            assert False, "unreachable!"
+
     print("hit: ", hit)
     print("branch_num: ", branch_num)
     print("stage_num: ", stage_num)
