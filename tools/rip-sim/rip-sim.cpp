3d7f3e59bcb921c0d22dd629441a1e280479e40f
khei4 <kk.asano.luxy@gmail.com>
2023-12-12 15:38:29 +0900

    fix: ASan crash on dhry

--- tools/rip-sim/rip-sim.cpp
+++ tools/rip-sim/rip-sim.cpp
@@ -156,8 +156,9 @@ int main(int argc, char **argv) {
   }
   DEBUG_ONLY(Stats = std::make_unique<Statistics>(););
 
-  RIPSimulator RipSim(Files, std::move(BP), Ops.getDRAMSize(),
-                      std::move(Stats));
+  RIPSimulator RipSim(Files, std::move(BP), Ops.getDRAMSize(), std::move(Stats),
+                      /*DRAMBase = */ 0x8000,
+                      /*SPIValue = */ 1 << 25);
 
   if (Ops.getInteractive())
     RipSim.runInteractively(Ops.getStartAddress(), Ops.getEndAddress());
