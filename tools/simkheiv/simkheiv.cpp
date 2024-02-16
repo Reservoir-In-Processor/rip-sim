668953edb39fb03549343d756ad50c187500622d
khei4 <kk.asano.luxy@gmail.com>
2024-02-16 17:33:11 +0900

    change config for cli

--- tools/simkheiv/simkheiv.cpp
+++ tools/simkheiv/simkheiv.cpp
@@ -12,9 +12,12 @@ int main(int argc, char **argv) {
   const std::string FileName = argv[1];
   std::string BaseNoExt = FileName.substr(0, FileName.find_last_of('.'));
   auto Files = std::ifstream(FileName);
-  Simulator Sim(Files);
+  Simulator Sim(Files, /*DRAMSize = */ 1LL << 28,
+                /* DRAMBase = */ 0x0000,
+                /* SPIvalue = */ 1LL << 25);
   Sim.run();
   Sim.dumpGPRegs();
+  Sim.getCSRs().dump();
 
   return 0;
 }
